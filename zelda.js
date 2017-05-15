5d48c3bd305ca2ba788cd8363db4d17897a46acb
Kendrick Roy <roykendrick934@gmail.com>
2017-05-15 21:48:24 +0000

    i added movement

--- zelda.js
+++ zelda.js
@@ -1,9 +1,14 @@
 $("body").keydown(function(event) {
     if (event.which === 37) {
-        $("#link").css("left", $("#link").position().left - 10);
+        event.preventDefault();
+        var leftTouch = $("#link").position().left - 10;
+        leftTouch = Math.max(leftTouch, 0)
+        $("#link").css("left", leftTouch);
     } else if (event.which === 39) {
         $("#link").css("left", $("#link").position().left + 10);
-    } else {
-        return;
-    }
+    }  else if (event.which === 38) {
+        $("#link").css("top", $("#link").position().top - 10);
+    }  else if (event.which === 40) {
+        $("#link").css("top", $("#link").position().top + 10);
+       }
 });
