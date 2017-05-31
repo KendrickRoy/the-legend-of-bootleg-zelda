f1d7388f23ca4e28613b59d0238d6905b691923c
Kendrick Roy <roykendrick934@gmail.com>
2017-05-31 21:48:34 +0000

    bbbuu

--- zelda.js
+++ zelda.js
@@ -1,22 +1,41 @@
+function checkCollision(adjustleft, adjusttop) {
+    var linkLeft = $("#link").offset().left + adjustleft;
+    var rockLeft = $("#rock").offset().left;
+    var linkRight = linkLeft + $("#link").width();
+    var rockRight = rockLeft + $("#rock").width();
+    var linktop = $("#link").offset().top + adjusttop;
+    var rocktop = $("#rock").offset().top;
+    var rockBottom = rocktop + $("#rock").height();
+    var linkBottom = linktop + $("#link").height();
+    
+    if(linkRight >= rockLeft && linkLeft <= rockRight && linkBottom >= rocktop && linktop <= rockBottom) {
+        return true;
+    }else{
+        return false
+    }
+        
+}
 $("body").keydown(function(event) {
     var linkPosition = $('#link').position();
     $('#where-is-link').html("X: " + linkPosition.top + ', Y: ' + linkPosition.left);
     console.log('I AM GOING WITH ', event.which);
-    if (event.which === 37) {
+    if (event.which === 37 && !checkCollision(-10, 0)) {
         event.preventDefault();
         var leftTouch = $("#link").position().left - 10;
         leftTouch = Math.max(leftTouch, 0)
         //moves left
         $("#link").css("left", leftTouch);
-    } else if (event.which === 39) {
+        checkCollision()
+    } else if (event.which === 39 && !checkCollision(10, 0)) {
        event.preventDefault();
         var leftTouch = $("#link").position().left + 10;
         var boxWidth = $('#play-area').width();
         var linkwidth = $('#link').width();
         leftTouch = Math.min(leftTouch, boxWidth - linkwidth)
         //moves right
-        $("#link").css("left", leftTouch);
-    }  else if (event.which === 38) {
+        $("#link").css("left", leftTouch);;
+         checkCollision()
+    }  else if (event.which === 38 && !checkCollision(0, -10)) {
         var leftTouch = $("#link").position().left - 10;
         var topTouch = $("#link").position().top - 10;
         console.log(topTouch, leftTouch, event.which);
@@ -26,7 +45,8 @@ $("body").keydown(function(event) {
         //moves up
         $("#link").css("top", $("#link").position().top - 10);
         $("#link").css("top", topTouch);
-    }  else if (event.which === 40) {
+         checkCollision()
+    }  else if (event.which === 40 && !checkCollision(0, 10)) {
         var leftTouch = $("#link").position().left - 10;
         var topTouch = $("#link").position().top - 10;
         if (topTouch > 760 + 62){
@@ -38,5 +58,6 @@ $("body").keydown(function(event) {
         topTouch = Math.min(leftTouch, boxheight - linkheight)
         //moves down
         $("#link").css("top", $("#link").position().top + 10);
+         checkCollision()
        }
 });
